1405a3fb34cbc3002746a45d2338fb121fd6c8e4
supto11 <codersupto@gmail.com>
2021-06-21 02:56:09 +0600

    setup api overview

--- post/views.py
+++ post/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def api_overview(request):
+    api_urls = {
+        'All Posts': 'posts',
+        'Create': 'post/create',
+        'Update': 'post/<str:pk>',
+        'Delete': 'post/<str:pk>',
+    }
+    return Response(api_urls)
